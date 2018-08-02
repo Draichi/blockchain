f44feb42a0bffd461347258812468a00261c2a63
draichi <lucasdraichi@gmail.com>
2018-08-02 15:39:16 -0300

    fixing binding bug

--- run.py
+++ run.py
@@ -15,4 +15,4 @@ if __name__ == '__main__':
         print("Usage: python3 run.py [PORT]")
         exit()
     port = int(sys.argv[1])
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='127.0.0.1', port=port)
